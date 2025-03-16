a307b8d6c50b402b2a2458568a550f734c41bae6
oussamabartil <oussamabartil.04@gmail.com>
2025-03-16 18:45:17 +0000

    Configurer l'enverenomment de travail

--- app/src/main/java/com/example/tp4/feu3/ui/Feu3View.kt
+++ app/src/main/java/com/example/tp4/feu3/ui/Feu3View.kt
@@ -1,18 +1,16 @@
 package com.example.tp4.feu3.ui
 
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.*
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.*
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tp4.feu3.controller.Feu3ViewModel
 import com.example.tp4.feu3.state.Feu3State
-
 
 class Feu3View {
 
@@ -20,16 +18,24 @@ class Feu3View {
 
 @Composable
 fun MainActivityFeu3View(viewmodel: Feu3ViewModel = viewModel()) {
-// état auquel sabonne cette fonction composable
-
+    // état auquel s'abonne cette fonction composable
     val state = viewmodel.state
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-// affichage du feu, version 1
+        // affichage du feu, version 1
         Feu3ViewV1(state, modifier = Modifier.padding(16.dp))
-// bouton, voir la suite du TP
+        Button(
+            onClick = {
+                viewmodel.suivant() // modif par le contrôleur
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp)
+        ) {
+            Text(text = "état suivant")
+        }
     }
 }
 
